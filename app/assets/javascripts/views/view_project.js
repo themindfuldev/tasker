3e769e78bbb896288c4fcf62c34aa0f874178ebf
Tiago Garcia <tiagoromerogarcia@gmail.com>
2013-07-17 23:39:07 -0300

    implemented buttons for project

--- app/assets/javascripts/views/view_project.js
+++ app/assets/javascripts/views/view_project.js
@@ -1,6 +1,10 @@
 App.Views.ViewProject = Backbone.View.extend({
 	name: 'view_project',
 	
+	events : {
+		'click a[data-action=delete]': 'remove'
+	},
+	
 	render : function() {
 		var template = HandlebarsCompiler.get(this.name);
 		this.$el.html(template(this.model.toJSON()));
@@ -17,6 +21,18 @@ App.Views.ViewProject = Backbone.View.extend({
 			model : card 
 		});
 		this.$el.append(viewStoryView.render());
+	},
+	
+	remove : function() {
+		this.model.remove({
+			success: function(model, response, options) {
+				App.Helpers.alert('Card ' + model.attributes.title + ' removido com sucesso!', 'success');
+			},
+			
+			error: function(model, response, options) {
+				App.Helpers.alert('Houve um erro ao remover o card!', 'error');
+			}
+		});
 	}
 
 

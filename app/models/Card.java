9e4169b22a63dce3915235df63a8b217acb6c2a3
Tiago Garcia <tiagoromerogarcia@gmail.com>
2013-08-11 19:07:43 -0300

    language gear + orderby issues

--- app/models/Card.java
+++ app/models/Card.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 
 import play.db.ebean.Model;
 
@@ -60,6 +61,7 @@ public class Card extends Model {
 	private Card parent;
 
 	@OneToMany(mappedBy = "parent", cascade = { CascadeType.REMOVE })
+	@OrderBy("modifiedDate")
 	private List<Card> children;
 
 	/*
